09e76f299b799a4bef2772cdf877cc619e96f077
Dakota Lillie <dakota.lillie@icloud.com>
2018-04-08 21:10:15 -0400

    basic login form

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,11 +1,37 @@
 import React from 'react';
+import { Button, Container, Form, FormGroup, Input, Label } from 'reactstrap';
 import SiteHeader from '../shared/siteHeader/SiteHeader';
+import './login.css';
 
 const Login = props => {
   return (
     <div className="login">
       <SiteHeader />
-      This is the login page
+      <Container>
+        <div className="login-form">
+          <Form>
+            <FormGroup>
+              <Label for="exampleEmail">Email</Label>
+              <Input
+                type="email"
+                name="email"
+                id="exampleEmail"
+                placeholder="your@email.com"
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="examplePassword">Password</Label>
+              <Input
+                type="password"
+                name="password"
+                id="examplePassword"
+                placeholder="pass123"
+              />
+            </FormGroup>
+            <Button>Submit</Button>
+          </Form>
+        </div>
+      </Container>
     </div>
   );
 };
